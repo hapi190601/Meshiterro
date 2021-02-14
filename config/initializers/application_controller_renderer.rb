f0689500fc26b321203f4a88278be269ffd837ae
hapi190601 <gc1206ro@gmail.com>
2021-02-14 10:51:11 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e9235a92f8e4bb86c2ff2adcbbc9616a20e22007eaae9de7edf9d8920a188fad48dcda125fb3a2bb152b0795e70c46f36543a67b8e2a942a993effed63548e0f'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  # root to: "homes#top"
+  root 'post_images#index'
   devise_for :users
-  root to: "homes#top"
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end
